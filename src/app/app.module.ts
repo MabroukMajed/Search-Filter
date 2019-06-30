5dca6fd1a467429e6e4cce6b74865ddd84c0b911
Mabrouk Majed <mejedmabrouk@gmail.com>
2019-06-30 11:54:50 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+// search module
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppComponent } from './app.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SearchFilterComponent],
+  imports: [BrowserModule, FormsModule, Ng2SearchPipeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
